88d984f8cd059b046fc13266497c7a577a958f2e
Kiki Dolan <cdolan323@gmail.com>
2018-09-13 23:42:50 +0000

    automatically backed up by learn

--- week-10/index.js
+++ week-10/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   document.addEventListener('submit', (event) => {
     const input = document.getElementById('ticketmaster-search')
+    const value = input.value
+    fetch()
   })
 })
