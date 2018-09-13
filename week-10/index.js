1702e6e7a7fbc16f3cd04339fce1ac5aaee3a593
Kiki Dolan <cdolan323@gmail.com>
2018-09-13 23:50:01 +0000

    automatically backed up by learn

--- week-10/index.js
+++ week-10/index.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   document.addEventListener('submit', (event) => {
     const input = document.getElementById('ticketmaster-search')
     const value = input.value
-    fetch()
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${value}&apikey=${API_Key}`)
   })
 })
