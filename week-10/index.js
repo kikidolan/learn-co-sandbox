ef1cc91bdf1b360761aa06a8557f6445d5abcfcb
Kiki Dolan <cdolan323@gmail.com>
2018-09-14 00:27:21 +0000

    automatically backed up by learn

--- week-10/index.js
+++ week-10/index.js
@@ -1,14 +1,13 @@
 const API_Key = 'Gafga3uOF3HGeKGwKW3xEo0rZP3DN32A'
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  navigator.geolocation.getCurrentPosition((position) =>
-  
+
   document.addEventListener('submit', (event) => {
     const input = document.getElementById('ticketmaster-search')
     const value = input.value
     fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${value}&apikey=${API_Key}`)
     .then(response => response.json())
-    .then(responseJson = console.log(responseJson))
+    .then(responseJson = makeEvents(responseJson))
   })
   })
 
